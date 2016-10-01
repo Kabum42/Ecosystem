d1522b59c4efad7649fca775da7ed98e8e7bcf6b
Kabum42 <kabum42@gmail.com>
2016-10-01 03:41:45 +0200

    Initial structure

---
+++ Assets/Code/Message.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Message {
+
+	public string sender;
+	public string information;
+
+}
+
+public class Letter : Message {
+
+	public Letter(string auxSender, string auxInformation) {
+
+		sender = auxSender;
+		information = auxInformation;
+
+	}
+
+}
+
+public class Speech : Message {
+
+	public Speech(string auxSender, string auxInformation) {
+
+		sender = auxSender;
+		information = auxInformation;
+
+	}
+
+}
