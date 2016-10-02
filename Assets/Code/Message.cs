fe28d86b8a0995c0ac43fd2465f52e9f07ab0708
Kabum42 <kabum42@gmail.com>
2016-10-02 15:35:45 +0200

    Editor more or less

--- Assets/Code/Message.cs
+++ Assets/Code/Message.cs
@@ -1,31 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public abstract class Message {
 
 	public string sender;
 	public string information;
+	public List<Option> options;
 
 }
 
 public class Letter : Message {
 
-	public Letter(string auxSender, string auxInformation) {
+	public Stamp stamp;
+
+	public Letter(string auxSender, string auxInformation, List<Option> auxOptions, Stamp auxStamp) {
 
 		sender = auxSender;
 		information = auxInformation;
+		options = auxOptions;
+		stamp = auxStamp;
 
+	}
+
+	public enum Stamp {
+		TopSecret,
+		Forbidden
 	}
 
 }
 
 public class Speech : Message {
 
-	public Speech(string auxSender, string auxInformation) {
+	public Speech(string auxSender, string auxInformation, List<Option> auxOptions) {
 
 		sender = auxSender;
 		information = auxInformation;
+		options = auxOptions;
 
+	}
+
+}
+
+public class Option {
+
+	public string text;
+	public List<Consequence> consequences;
+
+	public Option(string auxText, List<Consequence> auxConsequences) {
+
+		text = auxText;
+		consequences = auxConsequences;
+
+	}
+
+}
+
+public class Consequence {
+
+	public Type type;
+	public float change = 0;
+
+	public Consequence(Type auxType, float auxChange) {
+		type = auxType;
+		change = auxChange;
+	}
+
+	public enum Type {
+		bee,
+		flower
 	}
 
 }
