b74421e4785db5cdeed5b85e10a36476cf7379ad
Kabum42 <kabum42@gmail.com>
2016-10-04 01:49:27 +0200

    Message Crafter updated

---
+++ Assets/Code/MessageCrafter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageCrafter : MonoBehaviour {
+
+	public RectTransform parentUI;
+	private static float speed = 200f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		handleNavigation ();
+	
+	}
+
+	void handleNavigation() {
+
+		if (Input.GetKey (KeyCode.UpArrow) && parentUI.anchoredPosition3D.y > 0f) {
+			parentUI.position = parentUI.position + new Vector3 (0f, -Time.deltaTime * speed, 0f);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			parentUI.position = parentUI.position + new Vector3 (0f, Time.deltaTime * speed, 0f);
+		}
+
+
+
+	}
+
+}
