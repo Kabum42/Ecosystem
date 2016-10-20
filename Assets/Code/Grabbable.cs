729eab38d8305821340546140eb805dd95858fe1
DavidEmetres <david.martinez.martin.1995@gmail.com>
2016-10-20 19:21:57 +0200

    GOTY 2

---
+++ Assets/Code/Grabbable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabbable : MonoBehaviour {
+
+	ObjectGrabber objectGrabber;
+
+	public Vector3 rotationGrabbed;
+	public Vector3 positionGrabbed;
+
+	void Start () {
+
+		objectGrabber = Camera.main.gameObject.GetComponent<ObjectGrabber> ();
+
+	}
+
+	void Update () {
+
+		if (Hacks.isOver (this.gameObject)) {
+			if (Input.GetMouseButtonDown (0)) {
+				objectGrabber.Grab (this.gameObject);
+			}
+		}
+
+	}
+
+}
