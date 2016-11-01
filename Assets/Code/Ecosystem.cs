559bf6735076d2bae294fcf7614a9a12420b6084
Kabum42 <kabum42@gmail.com>
2016-11-01 23:03:35 +0100

    First poblation equlibrium achieved

--- Assets/Code/Ecosystem.cs
+++ Assets/Code/Ecosystem.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public static class Ecosystem {
 
 	private static bool started = false;
-	private static List<SpeciesData> speciesDataList = new List<SpeciesData>();
+
+	public static float deathRate = 1f/100f;
+	public static float cleaning = 100f/100f;
+	public static List<SpeciesData> speciesDataList = new List<SpeciesData>();
 
 	public static void Start() {
 		if (!started) {
@@ -18,34 +21,197 @@ public static class Ecosystem {
 
 		SpeciesData sp;
 
-		sp = new SpeciesData (Species.Bear);
+		//TREE
+		sp = new SpeciesData(Species.Tree, 20f);
+		sp.populationCap = 1000;
+		sp.AddBooster (Species.Bee);
+		speciesDataList.Add (sp);
+
+		//BUSH
+		sp = new SpeciesData(Species.Bush, 20f);
+		sp.populationCap = 2000;
+		sp.AddBooster (Species.Bee);
+		speciesDataList.Add (sp);
+
+		//BEE
+		sp = new SpeciesData(Species.Bee, 20f);
+		sp.populationCap = 3000;
+		sp.AddBooster (Species.Tree);
+		sp.AddBooster (Species.Bush);
+		speciesDataList.Add (sp);
+
+		//RABBIT
+		sp = new SpeciesData(Species.Rabbit, 20f);
+		sp.AddPrey (Species.Bush);
+		speciesDataList.Add (sp);
+
+		//DEER
+		sp = new SpeciesData(Species.Deer, 5f);
+		sp.AddPrey (Species.Bush);
+		speciesDataList.Add (sp);
+
+		//WOLF
+		sp = new SpeciesData(Species.Wolf, 5f);
+		sp.AddPrey (Species.Rabbit);
+		sp.AddPrey (Species.Deer);
+		speciesDataList.Add (sp);
+
+		//BEAR
+		sp = new SpeciesData(Species.Bear, 1.1f);
+		sp.AddPrey (Species.Bush);
+		sp.AddPrey (Species.Bee);
+		sp.AddPrey (Species.Rabbit);
+		sp.AddPrey (Species.Salmon);
+		speciesDataList.Add (sp);
+
+		//SALMON
+		sp = new SpeciesData(Species.Salmon, 20f);
+		sp.populationCap = 100;
+		speciesDataList.Add (sp);
 
 
 	}
 
 
 	public static SpeciesData GetSpeciesData(Species species) {
+
+		foreach (SpeciesData sp in speciesDataList) {
+			if (sp.species == species) {
+				return sp;
+			}
+		}
+
 		return null;
 	}
-		
+
+
+	public static void Simulate() {
+
+		foreach (SpeciesData sp in speciesDataList) {
+			sp.Grow ();
+		}
+
+		foreach (SpeciesData sp in speciesDataList) {
+			if (sp.populationCap >= 0 && sp.population > sp.populationCap) {
+				sp.population = sp.populationCap;
+			}
+		}
+
+	}
 
 }
 
 public class SpeciesData {
 
 	public Species species;
+	public float population = 100f;
+	public float reproductionRate = 0f;
+	public int populationCap = -1;
 
-	public SpeciesData(Species auxSpecies) {
+	private List<Species> preys = new List<Species> ();
+	private List<Species> boosters = new List<Species> ();
+
+	public SpeciesData(Species auxSpecies, float auxReproductionRate) {
 
 		species = auxSpecies;
+		reproductionRate = auxReproductionRate;
+
+	}
+
+	public void AddPrey(Species prey) {
+
+		preys.Add (prey);
+
+	}
+
+	public void AddBooster(Species booster) {
+
+		boosters.Add (booster);
+
+	}
+
+	private float Predate() {
+
+		float foodModifier = 0f;
+
+		if (preys.Count == 0 && boosters.Count == 0) {
+			
+			foodModifier = 1f;
+
+		} else {
+			
+			foreach (Species prey in preys) {
+
+				float weight = 1f / ((float)preys.Count + (float) boosters.Count);
+				SpeciesData preyData = Ecosystem.GetSpeciesData (prey);
+
+				float wantToEat = population * weight;
+				float eat = wantToEat;
+
+				if (eat > preyData.population * preyData.reproductionRate/100f *0.1f) {
+					eat = preyData.population * preyData.reproductionRate/100f *0.1f;
+				}
+
+				preyData.population -= eat;
+				foodModifier += (eat/wantToEat)*weight;
+
+			}
+
+			foreach (Species booster in boosters) {
+
+				float weight = 1f / ((float)preys.Count + (float) boosters.Count);
+				SpeciesData boosterData = Ecosystem.GetSpeciesData (booster);
+
+				float wantToEat = population * weight * 0.1f;
+				float eat = wantToEat;
+
+				if (eat > boosterData.population) {
+					eat = boosterData.population;
+				}
+					
+				// BOOSTERS'POPULATION ISN'T REDUCED IN THE PROCESS
+				foodModifier += (eat/wantToEat)*weight;
+
+			}
+
+		}
+
+		return foodModifier;
+
+	}
+
+	public void Grow() {
+
+		float populationChange = 0f;
+
+		// REPRODUCTION
+		if (population >= 2) {
+			float foodModifier = Predate();
+			populationChange += (reproductionRate/100f) * foodModifier;
+		}
+
+		// DEATH
+		populationChange -= Ecosystem.deathRate;
+
+		population += population * populationChange;
+
+		if (population < 0) {
+			population = 0;
+		}
 
 	}
 
 }
 
 public enum Species {
+	Tree,
+	Bush,
+	Bee,
+	Rabbit,
+	Deer,
+	Wolf,
 	Bear,
-	Flower
+	Salmon
 }
 
 
---
+++ Assets/Code/TestChangeReproduction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TestChangeReproduction : MonoBehaviour {
+
+	public float simulationRate = 1f;
+	private float simulationAux = 0f;
+	public Text textSource;
+	public List<Text> texts = new List<Text> ();
+	public List<float> reproductionRates = new List<float>();
+
+	// Use this for initialization
+	void Start () {
+	
+		Ecosystem.Start ();
+
+		int i = 0;
+
+		foreach (SpeciesData sp in Ecosystem.speciesDataList) {
+			Text t = Instantiate (textSource);
+			t.transform.SetParent (textSource.transform.parent);
+			t.transform.localScale = textSource.transform.localScale;
+			t.rectTransform.anchoredPosition = new Vector2 (0f, -30f -30f*i);
+			t.gameObject.SetActive (true);
+			texts.Add (t);
+			reproductionRates.Add (sp.reproductionRate);
+			i++;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		simulationAux += Time.deltaTime;
+		if (simulationAux > simulationRate) {
+			simulationAux -= simulationRate;
+			Ecosystem.Simulate ();
+		}
+
+		for (int i = 0; i < Ecosystem.speciesDataList.Count; i++) {
+			Ecosystem.speciesDataList [i].reproductionRate = reproductionRates [i];
+			texts [i].text = Ecosystem.speciesDataList [i].species.ToString () + ": " + Mathf.Floor(Ecosystem.speciesDataList [i].population);
+		}
+
+	}
+}
