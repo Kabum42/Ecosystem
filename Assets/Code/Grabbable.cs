c6602d7f5d657a16d9595194370e0a34409496eb
Kabum42 <kabum42@gmail.com>
2016-11-13 22:04:13 +0100

    Cursors working

--- Assets/Code/Grabbable.cs
+++ Assets/Code/Grabbable.cs
@@ -8,6 +8,9 @@ public class Grabbable : MonoBehaviour {
 	public Vector3 rotationGrabbed;
 	public Vector3 positionGrabbed;
 
+	private static float lastTimeChecked = 0f;
+	private static string consensusTexture = "none";
+
 	void Start () {
 
 		objectGrabber = Camera.main.gameObject.GetComponent<ObjectGrabber> ();
@@ -17,10 +20,58 @@ public class Grabbable : MonoBehaviour {
 	void Update () {
 
 		if (Hacks.isOver (this.gameObject)) {
+
+			setHand ();
+
 			if (Input.GetMouseButtonDown (0)) {
 				objectGrabber.Grab (this.gameObject);
 			}
+		} else {
+
+			setArrow ();
+
 		}
+
+	}
+
+	void setHand() {
+
+		if (lastTimeChecked == Time.time) {
+
+			consensusTexture = "hand";
+
+		} else {
+
+			setPrevious ();
+
+			consensusTexture = "hand";
+			lastTimeChecked = Time.time;
+
+		}
+
+	}
+
+	void setArrow() {
+
+		if (lastTimeChecked == Time.time) {
+
+
+
+		} else {
+
+			setPrevious ();
+
+			consensusTexture = "arrow";
+			lastTimeChecked = Time.time;
+
+		}
+
+	}
+
+	void setPrevious() {
+
+		Texture2D cursorTexture = Resources.Load ("2d/cursor_"+consensusTexture, typeof(Texture2D)) as Texture2D;
+		Cursor.SetCursor (cursorTexture, Vector2.zero, CursorMode.Auto);
 
 	}
 
